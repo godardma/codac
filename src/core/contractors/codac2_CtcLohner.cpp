5449c1fc9cc72b972ef75b5ed4a1056edefabd49
godardma <mael.godard@ensta-bretagne.org>
2026-02-20 18:28:07 +0100

    [Lohner] compiles but not working

---
+++ src/core/contractors/codac2_CtcLohner.cpp
@@ -0,0 +1,144 @@
+/** 
+ *  codac2_CtcLohner.cpp
+ * ----------------------------------------------------------------------------
+ *  \date       2026
+ *  \author     Auguste Bourgois, Maël Godard
+ *  \copyright  Copyright 2025 Codac Team
+ *  \license    GNU Lesser General Public License (LGPL)
+ */
+
+#include "codac2_CtcLohner.h"
+#include "codac2_Slice.h"
+
+// TO DELETE
+#include <iostream>
+
+using namespace std;
+using namespace codac2;
+
+LohnerAlgorithm::LohnerAlgorithm(const AnalyticFunction<VectorType> *f,
+                                 double h,
+                                 bool forward,
+                                 const IntervalVector &u0,
+                                 int contractions,
+                                 double eps)
+    : dim(f->input_size()),
+      h(h),
+      direction((forward) ? FWD : BWD),
+      eps(eps),
+      contractions(contractions),
+      u(u0),
+      z(u0 - u0.mid()),
+      r(z),
+      B(Matrix::eye(dim,dim)),
+      Binv(Matrix::eye(dim,dim)),
+      u_hat(u0.mid()),
+      f(f) {}
+
+const IntervalVector &LohnerAlgorithm::integrate(unsigned int steps, double H) {
+  if (H > 0) h = H;
+  for (unsigned int i = 0; i < steps; ++i) {
+    auto z1 = z, r1 = r, u1 = u;
+    auto B1 = B, B1inv = Binv;
+    auto u_hat1 = u_hat;
+    IntervalVector u_t = globalEnclosure(u, FWD);
+    for (int j = 0; j < contractions; ++j) {
+      z1 = 0.5 * h * h * f->diff(u_t) * f->eval(u_t);
+      Vector m1 = z1.mid();
+      IntervalMatrix A = Matrix::eye(dim,dim) + h * direction * f->diff(u);
+      Eigen::HouseholderQR<Eigen::MatrixXd> qr((A * B.template cast<Interval>()).mid());
+      B1 = qr.householderQ();
+      B1inv = B1.inverse();
+      r1 = (B1inv.template cast<Interval>() * A * B.template cast<Interval>()) * r + B1inv.template cast<Interval>() * (z1 - m1);
+      u_hat1 = u_hat + h * direction * f->eval(u_hat).mid() + m1;
+      u1 = u_hat1 + B1 * r1;
+      if (j < contractions - 1) {
+        u_t = u_t & globalEnclosure(u1, BWD);
+      }
+    }
+    z = z1, r = r1, u = u1, B = B1, Binv = B1inv, u_hat = u_hat1, u_tilde = u_t;
+  }
+  return u;
+}
+
+IntervalVector LohnerAlgorithm::globalEnclosure(const IntervalVector &initialGuess, double dir) {
+  IntervalVector u_0 = initialGuess;
+  for (unsigned int i = 0; i < 30; ++i) {
+    IntervalVector u_1 = initialGuess + dir * direction * Interval(0, h) * f->eval(u_0);
+    cout << "Global enclosure iteration " << i << ": " << u_1 << endl;
+    cout << "Global enclosure iteration " << i << " (initial guess): " << u_0 << endl<< endl;
+    if (u_0.is_superset(u_1)) {
+      return u_0;
+    } else {
+      u_0 = (1 + eps) * u_1 - eps * u_1;
+    }
+  }
+  throw GlobalEnclosureError();
+}
+
+void LohnerAlgorithm::contractStep(const IntervalVector &x) {
+  u = x & u;
+  u_hat = u.mid();
+  r = r & (Binv * (u - u_hat));
+}
+
+const IntervalVector &LohnerAlgorithm::getLocalEnclosure() const {
+  return u;
+}
+
+const IntervalVector &LohnerAlgorithm::getGlobalEnclosure() const {
+  return u_tilde;
+}
+
+CtcLohner::CtcLohner(const AnalyticFunction<VectorType>& f, int contractions, double eps)
+  :   m_f(f),
+      contractions(contractions),
+      dim(f.input_size()),
+      eps(eps) {}
+
+void CtcLohner::contract(codac2::SlicedTube<IntervalVector> &tube, TimePropag t_propa) {
+  assert((!tube.is_empty()) && (tube.size() == dim));
+  IntervalVector input_gate = IntervalVector::constant(dim, Interval(0));
+  IntervalVector output_gate = IntervalVector::constant(dim, Interval(0));
+  IntervalVector slice = IntervalVector::constant(dim, Interval(0));
+  double h;
+  if ((t_propa & TimePropag::FWD) != static_cast<TimePropag>(0)) {
+    for (int j = 0; j < dim; ++j) {
+      input_gate[j] = tube[j].first_slice()->input_gate();
+    }
+    LohnerAlgorithm lo(&m_f, 0.1, true, input_gate, contractions, eps);
+    // Forward loop
+    // replace for (int i = 0; i < tube.nb_slices(); ++i) with iterator
+    for (auto it = tube[0].tdomain()->begin(); it != tube[0].tdomain()->end(); ++it) {
+      h = (*it).diam();
+      for (int j = 0; j < dim; ++j) {
+        output_gate[j] = tube[j].slice(it)->output_gate();
+        slice[j] = tube[j].slice(it)->codomain();
+      }
+      lo.integrate(1, h);
+      lo.contractStep(output_gate);
+      for (int j = 0; j < dim; ++j)
+        tube[j].slice(it)->set(slice[j] & lo.getGlobalEnclosure()[j]);  // set_envelope equivalent in codac2 ?
+    }
+    tube.set(output_gate & lo.getLocalEnclosure(), tube.tdomain()->t0_tf().ub());
+  }
+  if ((t_propa & TimePropag::BWD) != static_cast<TimePropag>(0)) {
+    for (int j = 0; j < dim; ++j) {
+      input_gate[j] = tube[j].last_slice()->output_gate();
+    }
+    LohnerAlgorithm lo2(&m_f, 0.1, false, input_gate, contractions, eps);
+    // Backward loop
+    for (auto it = tube[0].tdomain()->begin(); it != tube[0].tdomain()->end(); ++it) {
+      h = (*it).diam();
+      for (int j = 0; j < dim; ++j) {
+        output_gate[j] = tube[j].slice(it)->input_gate();
+        slice[j] = tube[j].slice(it)->codomain();
+      }
+      lo2.integrate(1, h);
+      lo2.contractStep(output_gate);
+      for (int j = 0; j < dim; ++j)
+        tube[j].slice(it)->set(slice[j] & lo2.getGlobalEnclosure()[j]);
+    }
+    tube.set(output_gate & lo2.getLocalEnclosure(), tube.tdomain()->t0_tf().lb());
+  }
+}
---
+++ src/core/contractors/codac2_CtcLohner.h
@@ -0,0 +1,129 @@
+/** 
+ *  \file codac2_CtcLohner.h
+ * ----------------------------------------------------------------------------
+ *  \date       2026
+ *  \author     Auguste Bourgois, Maël Godard
+ *  \copyright  Copyright 2025 Codac Team
+ *  \license    GNU Lesser General Public License (LGPL)
+ */
+
+#pragma once
+
+#include "codac2_TimePropag.h"
+#include "codac2_AnalyticFunction.h"
+#include "codac2_TDomain.h"
+#include "codac2_SlicedTube.h"
+
+namespace codac2
+{ 
+
+  /**
+   * \class GlobalEnclosureError
+   *
+   * \brief Encapsulates runtime error for global enclosure estimation failure
+   */
+  struct GlobalEnclosureError : public std::runtime_error {
+    GlobalEnclosureError() : std::runtime_error(
+        "Exceeded loop maximum range while looking for a global enclosure for the system.") {}
+  };
+
+  /**
+   * \class LohnerAlgorithm
+   *
+   * \brief Simple first order Lohner algorithm to perform guaranteed integration of a system
+   * \f$\dot{\mathbf{x}}=\mathbf{f}(\mathbf{x})\f$
+   */
+  class LohnerAlgorithm {
+  public:
+    constexpr static const double FWD = 1., BWD = -1.;
+
+    /**
+     * \brief Creates a Lohner algorithm object
+     *
+     * \param f function defining the system \f$\dot{\mathbf{x}}=\mathbf{f}(\mathbf{x})\f$
+     * \param h time step of the integration method
+     * \param direction forward or backaward integration
+     * \param u0 initial condition of the system
+     * \param contractions number of contractions of the global enclosure by the estimated local enclosure
+     * \param eps inflation parameter for the global enclosure
+     */
+    LohnerAlgorithm(const AnalyticFunction<VectorType> *f,
+                    double h,
+                    bool forward,
+                    const IntervalVector &u0 = IntervalVector::empty(1),
+                    int contractions = 1,
+                    double eps = 0.1);
+    /**
+     * \brief integrate the system over a given number of steps
+     *
+     * \param steps number of steps to integrate
+     * \param H parameter to overwrite the integration time step
+     * \return enclosure of the system's state
+     */
+    const IntervalVector &integrate(unsigned int steps, double H = -1);
+
+    /**
+     * \brief contract the global & local enclosure of the previous integration step
+     *
+     * \param x estimation of the global enclosure
+     */
+    void contractStep(const IntervalVector &x);
+
+    /**
+     * \brief Returns the current global enclosure, i.e. the box enclosing the trajectories between times \f$k-1\f$ and
+     * \f$k\f$
+     *
+     * \return global enclosure
+     */
+    const IntervalVector &getLocalEnclosure() const;
+
+    /**
+     * \brief Returns the current local enclosure, i.e. the box enclosing the trajectories at time \f$k\f$
+     *
+     * \return local enclosure \f$[\mathbf{x}_{k}]\f$
+     */
+    const IntervalVector &getGlobalEnclosure() const;
+  private:
+    /**
+     * \brief Computes an estimation of the global enclosure of the system
+     *
+     * \param initialGuess initial guess for the global enclosure
+     * \param dir forward or backward in time
+     * \return estimation of the global enclosure
+     */
+    IntervalVector globalEnclosure(const IntervalVector &initialGuess, double dir);
+
+    unsigned int dim; //!< dimension of the system's state
+    double h; //!< integration time step
+    double direction; //!< forward or backward integration
+    double eps; //!< inflation parameter for the global enclosure
+    int contractions; //!< number of contractions of the global enclosure by the estimated local enclosure
+    IntervalVector u; //!< local enclosure
+    IntervalVector z; //!< Taylor-Lagrange remainder (order 2)
+    IntervalVector r; //!< enclosure of uncertainties in the frame given by the matrix B
+    IntervalVector u_tilde; //!< global enclosure
+    Matrix B, Binv;
+    Vector u_hat; //!< center of the box u
+    const AnalyticFunction<VectorType> *f; //!< litteral function of the system \f$\dot{\mathbf{x}}=\mathbf{f}(\mathbf{x})\f$
+  };
+
+  class CtcLohner
+  {
+    public:
+
+      CtcLohner(const AnalyticFunction<VectorType>& f, int contractions = 5, double eps = 0.1);
+
+      void contract(SlicedTube<IntervalVector> &tube, TimePropag t_propa = TimePropag::FWD_BWD);
+
+
+    protected:
+
+      AnalyticFunction<VectorType> m_f; //!< forward function
+      int contractions; //!< number of contractions of the global enclosure by the estimated local enclosure
+      int dim; //!< dimension of the state vector
+      double eps; //!< inflation parameter for the global enclosure
+
+      static const std::string m_ctc_name; //!< class name (mainly used for CN Exceptions)
+      static std::vector<std::string> m_str_expected_doms;
+  };
+}
